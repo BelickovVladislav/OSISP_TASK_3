2fa56b068bac9d040cb39829a76c505c1109879b
Belikov Vlad <belickov.vlad1997@yandex.ru>
2018-06-27 13:34:39 +0300

    done task

--- OSISP_TASK_3/Form1.cs
+++ OSISP_TASK_3/Form1.cs
@@ -12,9 +12,41 @@ namespace OSISP_TASK_3
 {
     public partial class Form1 : Form
     {
+        private Graphics g;
+        private Pen pen;
+        private bool draw;
+        private int x, y;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = this.CreateGraphics();
+            pen = new Pen(Color.Black);
+            draw = false;
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            draw = true;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            draw = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (draw)
+            {
+                g.DrawLine(pen, x, y, e.X, e.Y);
+            }
+            x = e.X;
+            y = e.Y;
         }
     }
 }
